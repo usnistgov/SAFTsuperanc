191681eee992de7d4b0c2a3602f41a064dee7d23
Ian Bell <ian.bell@nist.gov>
2022-05-27 16:32:36 -0400

    Bug with PC-SAFT squashed, all systems go
    Just some issues with refining of the expansions remains

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 
 // Imports from boost
 #include <boost/multiprecision/cpp_bin_float.hpp>
@@ -37,21 +38,25 @@ inline auto linterp (const T& x, const T& y, std::size_t i, const double val) {
 
 /// A class for building an interpolator of critical points as a function of 1/m
 /// 
-/// Reduced arrays are in reduced form
+/// Arrays of temperatures and densities are in tilde-reduced form
 template<typename Ms>
 class CriticalPointsInterpolator {
 private:
     const Ms ms;
     std::vector<double> Ttilde, rhotilde, one_over_m;
 public:
-    CriticalPointsInterpolator(Ms & ms, double T0, double rho0) : ms(ms) {
+    CriticalPointsInterpolator(const Ms & ms, double T0, double rho0) : ms(ms) {
         double T = T0, rho = rho0;
-        for (auto m : ms) {
+        for (double m : ms) {
             auto model = get_model(m);
             std::tie(T, rho) = teqp::solve_pure_critical(model, T, rho);
+            if (!std::isfinite(T)){
+                throw std::invalid_argument("Critical point solving failed for m of " + std::to_string(m));
+            }
             Ttilde.push_back(T/epsilon_over_k_K);
             rhotilde.push_back(rho*N_A*pow(sigma_m, 3));
-            one_over_m.push_back(1 / m);
+            one_over_m.push_back(1/m);
+            std::cout << "[crit] " <<  m << "::" << T << "," << rho << std::endl;
         }
     };
     void to_json(const std::string path) {
@@ -74,7 +79,7 @@ public:
                     return std::make_tuple(linterp(one_over_m, Ttilde, i, 1/m), linterp(one_over_m, rhotilde, i, 1/m));
                 }
             }
-            throw std::invalid_argument("How did I get here for m of " + std::to_string(m));
+            throw std::invalid_argument("How did I get here for m of " + std::to_string(m) + "for critical point interpolation");
         }
     }
     auto get_Tcrhoc(double m) {
@@ -83,7 +88,15 @@ public:
     }
 };
 
-/// Trace away from the critical point down to a minimum temperature
+template <typename T>
+std::string to_string_with_precision(const T a_value, const int n = 16)
+{
+    std::ostringstream out; out.precision(n);
+    out << std::scientific << a_value;
+    return out.str();
+}
+
+/// Trace away from the critical point down to a very low temperature
 class VLETracer {
 public:
     using my_float = double;
@@ -150,18 +163,18 @@ public:
     }
     /// Linearly interpolate to get the tilde-scaled temperature and density
     auto interpolate_tilde(const double Ttilde_) {
-        const auto eps = std::numeric_limits<double>::epsilon();
+        constexpr auto eps = std::numeric_limits<double>::epsilon();
         // Ttilde vector is stored in decreasing value, starting at critical and going down
         if (Ttilde_ > Ttilde[0]*(1+eps) || Ttilde_ < Ttilde[Ttilde.size() - 1]*(1-eps)) {
             throw std::invalid_argument("Ttilde is out of range");
         }
         else {
             for (auto i = 0; i < Ttilde.size() - 1; ++i) {
-                if (Ttilde_ <= Ttilde[i]*(1+eps) && Ttilde_ >= Ttilde[i+1]*(1-eps)) {
+                if (Ttilde_ <= Ttilde[i]*(1+100*eps) && Ttilde_ >= Ttilde[i+1]*(1-100*eps)) {
                     return std::make_tuple(linterp(Ttilde, rhotildeL, i, Ttilde_), linterp(Ttilde, rhotildeV, i, Ttilde_));
                 }
             }
-            throw std::invalid_argument("How did I get here?");
+            throw std::invalid_argument("How did I get here? Value of Ttilde is " + to_string_with_precision(Ttilde_) + " and limits are [" + to_string_with_precision(Ttilde[0]) + "," + to_string_with_precision(Ttilde[1]) + "]");
         }
     }
 
@@ -198,7 +211,7 @@ int main(){
     // Build interpolation function for critical point temperature and density as a function of 1/m
     double Ttilde1 = 1.2757487256161069; // nondimensional temperature
     double rhotilde1 = 0.2823886223463809; // nondimensional density
-    auto Ms = Eigen::ArrayXd::LinSpaced(101, 1, 101);
+    Eigen::ArrayXd Ms = Eigen::ArrayXd::LinSpaced(100, 1, 100);
     auto ccrough = CriticalPointsInterpolator(Ms, Ttilde1*epsilon_over_k_K, rhotilde1/(N_A*pow(sigma_m, 3)));
     ccrough.to_json("PCSAFT_crit_pts_interpolation.json");
 
