a04fa0b01b17feb147f49cc12756e7a777a92ced
Ian Bell <ian.bell@nist.gov>
2022-06-14 16:40:24 -0600

    Move more into the header

--- implementation/PCSAFTsuperancillary_data.hpp
+++ implementation/PCSAFTsuperancillary_data.hpp
@@ -15,4 +15,20 @@ struct CompleteWInterval {
 	const std::vector<WInterval> intervals;
 };
 
+template<std::size_t N>
+auto buildmat() {
+    Eigen::Matrix<double, N + 1, N + 1> L; ///< Matrix of coefficients
+    for (int j = 0; j <= N; ++j) {
+        for (int k = j; k <= N; ++k) {
+            double p_j = (j == 0 || j == N) ? 2 : 1;
+            double p_k = (k == 0 || k == N) ? 2 : 1;
+            L(j, k) = 2.0 / (p_j * p_k * N) * cos((j * EIGEN_PI * k) / N);
+            // Exploit symmetry to fill in the symmetric elements in the matrix
+            L(k, j) = L(j, k);
+        }
+    }
+    return L;
+}
+const static Eigen::MatrixXd V16 = buildmat<16>();
+
 #include "domain.cpp"
